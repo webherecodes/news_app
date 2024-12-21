a0461fd6e61457335046840007c14daf3c686886
Manobalaarul <manobalaarul3@gmail.com>
2024-12-21 21:30:55 +0530

    bloc added

---
+++ lib/Home/bloc/news_bloc.dart
@@ -0,0 +1,14 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+part 'news_event.dart';
+part 'news_state.dart';
+
+class NewsBloc extends Bloc<NewsEvent, NewsState> {
+  NewsBloc() : super(NewsInitial()) {
+    on<NewsEvent>((event, emit) {
+      
+    });
+  }
+}
